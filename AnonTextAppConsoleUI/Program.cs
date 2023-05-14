cf001044d5f7c634acaaf015d0b90cf13f1d1e78
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-05-14 15:42:03 +0700

    add testing

--- AnonTextAppConsoleUI/Program.cs
+++ AnonTextAppConsoleUI/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             string IdDoksli = ClientAPI.createDocument("Dalang G30S PKI", "Dalang G30S PKI adalah S_______", "123").Result;
+            Console.WriteLine($"ID Doksli adalah {IdDoksli}");
+
+            TextDocument doksli = ClientAPI.getDocument(IdDoksli).Result;
+            Console.WriteLine(doksli.title);
+
+            Task task = ClientAPI.changeTitle(IdDoksli,"123","Dasar PKI");
+
+            doksli = ClientAPI.getDocument(IdDoksli).Result;
+            Console.WriteLine(doksli.title);
         }
     }
 }
