85eff8eff511bed729814a282787ff3ba36a5210
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-05-14 21:32:25 +0700

    Do unit test

---
+++ UnitTesting-Adam/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace UnitTesting_Adam;
+
+[TestClass]
+public class UnitTest1
+{
+    private SimpleDB simpleDB = new SimpleDB();
+    string id1, id2;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        this.id1 = simpleDB.CreateDocument("Doc 1", "Gaada isi");
+        this.id2 = simpleDB.CreateDocument("Doc 1", "Masih Gaada isi", "12345");
+    }
+
+    [TestMethod]
+    public void UTA1()
+    {
+        string id = "165917503";
+        Assert.IsTrue(simpleDB.CheckDocument(id1));
+    }
+
+    [TestMethod]
+    public void UTA2()
+    {
+        string password = "12345";
+        Assert.IsTrue(simpleDB.CheckDocument(id2,password));
+    }
+
+    [TestMethod]
+    public void UTA3()
+    {
+        string password = "1234567";
+        Assert.IsFalse(simpleDB.CheckDocument(id2, password));
+    }
+}
