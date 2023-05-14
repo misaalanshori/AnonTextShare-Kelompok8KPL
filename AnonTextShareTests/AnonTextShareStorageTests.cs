3c56d802723f1a33040c0a9b9c5820198fd4206c
M Isa Al Anshori <isabungsu@gmail.com>
2023-05-14 21:21:29 +0700

    Buat test case untuk method UpdateCollectionsTitle dari SimpleDB

--- AnonTextShareTests/AnonTextShareStorageTests.cs
+++ AnonTextShareTests/AnonTextShareStorageTests.cs
@@ -1,11 +1,32 @@
+using AnonTextShareStorage;
+
 namespace AnonTextShareTests
 {
     [TestClass]
-    public class AnonTextShareStorageTests
+    public class AnonTextShareStorage_SimpleDB_CollectionTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        SimpleDB db;
+        string col;
+
+        [TestInitialize]
+        public void InitTest()
         {
+            db = new SimpleDB();
+            col = db.CreateCollection("Atitle", new List<string>{ "Hello", "World"}, "password");
+        }
+
+        [TestMethod]
+        public void TestUpdateCollectionsTitle_CorrectPass()
+        {
+            db.UpdateCollectionTitle(col, "judul baru", "password");
+            Assert.AreEqual("judul baru", db.GetCollectionTitle(col));
+        }
+
+        [TestMethod]
+        public void TestUpdateCollectionsTitle_IncorrectPass()
+        {
+            db.UpdateCollectionTitle(col, "judul terbaru", "passwordsalah");
+            Assert.AreNotEqual("judul terbaru", db.GetCollectionTitle(col));
         }
     }
 }
