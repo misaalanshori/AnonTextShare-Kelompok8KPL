3a3349301e7ce2ae7f4bbb6a719e9d0dc2a077cb
nasrulilham <91175018+nasrulilham@users.noreply.github.com>
2023-05-14 17:22:34 +0700

    Pembatasan teks & judul, RunTimeConffiguration

---
+++ AnonTextShareAPI/AppConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace AnonTextShareAPI
+{
+    public class AppConfig
+    {
+        public RuntimeConfig config;
+        public const string txt = @"./content.json";
+        public AppConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                setDefault();
+                WriteConfigFile();
+            }
+        }
+
+        public RuntimeConfig ReadConfigFile()
+        {
+            string jsonText = File.ReadAllText(txt);
+            config = JsonSerializer.Deserialize<RuntimeConfig>(jsonText);
+            return config;
+        }
+
+        public void setDefault()
+        {
+            config = new RuntimeConfig(32, 128);
+        }
+
+        public void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string jsonText = JsonSerializer.Serialize<RuntimeConfig>(config, options);
+            File.WriteAllText(txt, jsonText);
+        }
+    }
+}
