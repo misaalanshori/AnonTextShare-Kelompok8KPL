2da72fac2d8f0c59d63c3c5c308f43a706c0a4e5
M Isa Al Anshori <isabungsu@gmail.com>
2023-05-14 21:54:31 +0700

    Buat Test Case untuk API PatchTitle (TextDocument)

--- AnonTextShareTests/AnonTextShareAPITests.cs
+++ AnonTextShareTests/AnonTextShareAPITests.cs
@@ -1,11 +1,47 @@
+using AnonTextShareAPI;
+using AnonTextShareAPI.Controllers;
+
 namespace AnonTextShareTests
 {
+    
+
     [TestClass]
     public class AnonTextShareAPITests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private TextDocumentController? _controller;
+        private string _doc;
+        [TestInitialize]
+        public void InitTest()
         {
+            _controller = new TextDocumentController();
+            _doc = _controller.Post("password", new TextDocument("A Title", "The Content")).Value;
         }
+
+        [TestMethod]
+        public void TestPatchTitle_InvalidID()
+        {
+            var result = _controller.PatchTitle("123456", "password", "newTitle");
+            Assert.IsInstanceOfType(result, typeof(Microsoft.AspNetCore.Mvc.NotFoundObjectResult));
+        }
+        [TestMethod]
+        public void TestPatchTitle_InvalidPassword()
+        {
+            var result = _controller.PatchTitle(_doc, "pass", "newTitle");
+            Assert.IsInstanceOfType(result, typeof(Microsoft.AspNetCore.Mvc.BadRequestObjectResult));
+        }
+        [TestMethod]
+        public void TestPatchTitle_WrongPassword()
+        {
+            var result = _controller.PatchTitle(_doc, "password123", "newTitle");
+            Assert.IsInstanceOfType(result, typeof(Microsoft.AspNetCore.Mvc.UnauthorizedObjectResult));
+        }
+        [TestMethod]
+        public void TestPatchTitle_Success()
+        {
+            var result = _controller.PatchTitle(_doc, "password", "newTitle");
+            Assert.IsInstanceOfType(result, typeof(Microsoft.AspNetCore.Mvc.OkResult));
+        }
+
+
     }
 }
