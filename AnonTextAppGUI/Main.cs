66cd16f2e18a3795c4c674405d617645f04bdacc
AdamRFaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-06-16 01:02:15 +0700

    GUI menu done

--- AnonTextAppGUI/Main.cs
+++ AnonTextAppGUI/Main.cs
@@ -12,9 +12,41 @@ namespace AnonTextAppGUI
             IController.s_ControllerGUI = controllerGUI;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            IController.s_ControllerGUI.MenuToCreateDocument();
+            if (radioButton1.Checked)
+            {
+                IController.s_ControllerGUI.MenuToViewDocument(textBox1.Text, textBox2.Text);
+            }
+            else if (radioButton2.Checked)
+            {
+                IController.s_ControllerGUI.MenuToViewCollection(textBox1.Text, textBox2.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please select the type.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                IController.s_ControllerGUI.MenuToCreateDocument();
+            }
+            else if (radioButton2.Checked)
+            {
+                IController.s_ControllerGUI.MenuToCreateCollection();
+            }
+            else
+            {
+                MessageBox.Show("Please select the type.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
