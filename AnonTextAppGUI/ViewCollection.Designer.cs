20081ee5a6875f5afb9a7bb3ab76089a59b0a57b
MaximusBayu <maximusbayu9b16@gmail.com>
2023-06-16 00:42:41 +0700

    Progress GUI view collection (Oleh Habli)

--- AnonTextAppGUI/ViewCollection.Designer.cs
+++ AnonTextAppGUI/ViewCollection.Designer.cs
@@ -45,9 +45,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 32F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(281, 9);
+            label1.Location = new Point(482, 18);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(213, 59);
+            label1.Size = new Size(371, 100);
             label1.TabIndex = 0;
             label1.Text = "Collection";
             // 
@@ -55,9 +56,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(85, 79);
+            label2.Location = new Point(146, 158);
+            label2.Margin = new Padding(5, 0, 5, 0);
             label2.Name = "label2";
-            label2.Size = new Size(25, 21);
+            label2.Size = new Size(44, 38);
             label2.TabIndex = 1;
             label2.Text = "ID";
             label2.Click += label2_Click;
@@ -66,86 +68,99 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(85, 115);
+            label3.Location = new Point(146, 230);
+            label3.Margin = new Padding(5, 0, 5, 0);
             label3.Name = "label3";
-            label3.Size = new Size(47, 21);
+            label3.Size = new Size(82, 38);
             label3.TabIndex = 2;
             label3.Text = "Judul";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(138, 115);
+            textBox2.Location = new Point(237, 230);
+            textBox2.Margin = new Padding(5, 6, 5, 6);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(437, 23);
+            textBox2.Size = new Size(746, 35);
             textBox2.TabIndex = 4;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(85, 148);
+            label4.Location = new Point(146, 296);
+            label4.Margin = new Padding(5, 0, 5, 0);
             label4.Name = "label4";
-            label4.Size = new Size(125, 21);
+            label4.Size = new Size(222, 38);
             label4.TabIndex = 5;
             label4.Text = "Daftar Dokumen";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(85, 177);
+            textBox3.Location = new Point(146, 354);
+            textBox3.Margin = new Padding(5, 6, 5, 6);
             textBox3.Multiline = true;
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(579, 249);
+            textBox3.Size = new Size(990, 494);
             textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(85, 444);
+            textBox4.Location = new Point(146, 888);
+            textBox4.Margin = new Padding(5, 6, 5, 6);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(409, 23);
+            textBox4.Size = new Size(698, 35);
             textBox4.TabIndex = 7;
+            textBox4.TextChanged += textBox4_TextChanged;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(151, 79);
+            label5.Location = new Point(259, 158);
+            label5.Margin = new Padding(5, 0, 5, 0);
             label5.Name = "label5";
-            label5.Size = new Size(127, 21);
+            label5.Size = new Size(227, 38);
             label5.TabIndex = 8;
             label5.Text = "< ID Dokumen >";
             // 
             // button1
             // 
-            button1.Location = new Point(500, 443);
+            button1.Location = new Point(857, 886);
+            button1.Margin = new Padding(5, 6, 5, 6);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(129, 46);
             button1.TabIndex = 9;
             button1.Text = "Add";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(581, 444);
+            button2.Location = new Point(996, 888);
+            button2.Margin = new Padding(5, 6, 5, 6);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(129, 46);
             button2.TabIndex = 10;
             button2.Text = "Delete";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(581, 113);
+            button3.Location = new Point(996, 226);
+            button3.Margin = new Padding(5, 6, 5, 6);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Size = new Size(129, 46);
             button3.TabIndex = 11;
             button3.Text = "Update Judul";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // ViewCollection
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 535);
+            ClientSize = new Size(1371, 1070);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -157,6 +172,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(5, 6, 5, 6);
             Name = "ViewCollection";
             Text = "ViewCollection";
             Load += ViewCollection_Load;
--- AnonTextAppGUI/ViewCollection.cs
+++ AnonTextAppGUI/ViewCollection.cs
@@ -30,11 +30,16 @@ namespace AnonTextAppGUI
             _collection = ClientAPI.getCollection(_id).Result;
             this.textBox2.Text = _collection.title;
             this.label5.Text = _collection.id;
-            
+
             for (int i = 0; i < _collection.contents.Count; i++)
             {
                 this.textBox3.Text += _collection.id + " - " + _collection.title + "\n";
             }
+        }
+
+        public void DeleteData()
+        {
+            ClientAPI.deleteDocument(_id,_password);
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -45,6 +50,33 @@ namespace AnonTextAppGUI
         private void ViewCollection_Load(object sender, EventArgs e)
         {
             RefreshData();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length > 0)
+            {
+                this.textBox3.Text = _collection.id + " - " + _collection.title + "\n";
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Length > 0)
+            {
+                this.textBox3.Text = _collection.id + " - " + _collection.title + "\n";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DeleteData();
         }
     }
 }
