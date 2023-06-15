2831e7055ab624cd9d6b826fbc6f1e2cdd18787e
M Isa Al Anshori <isabungsu@gmail.com>
2023-06-15 16:24:22 +0700

    GUI view collection (Oleh Habli)

--- AnonTextAppGUI/ViewCollection.Designer.cs
+++ AnonTextAppGUI/ViewCollection.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ViewCollection";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 32F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(281, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(213, 59);
+            label1.TabIndex = 0;
+            label1.Text = "Collection";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(85, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 21);
+            label2.TabIndex = 1;
+            label2.Text = "ID";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(85, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Judul";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(138, 115);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(437, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(85, 148);
+            label4.Name = "label4";
+            label4.Size = new Size(125, 21);
+            label4.TabIndex = 5;
+            label4.Text = "Daftar Dokumen";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(85, 177);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(579, 249);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(85, 444);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(409, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(151, 79);
+            label5.Name = "label5";
+            label5.Size = new Size(127, 21);
+            label5.TabIndex = 8;
+            label5.Text = "< ID Dokumen >";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(500, 443);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 9;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(581, 444);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(581, 113);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 11;
+            button3.Text = "Update Judul";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // ViewCollection
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 535);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "ViewCollection";
+            Text = "ViewCollection";
+            Load += ViewCollection_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- AnonTextAppGUI/ViewCollection.cs
+++ AnonTextAppGUI/ViewCollection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnonTextAppConsoleUI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,38 @@ namespace AnonTextAppGUI
 {
     public partial class ViewCollection : Form
     {
-        public ViewCollection()
+
+        private string _id;
+        private string _password;
+        private TextCollection _collection;
+        public ViewCollection(string IdCollection, string PasswordCollection)
         {
+            _id = IdCollection;
+            _password = PasswordCollection;
+            _collection = ClientAPI.getCollection(IdCollection).Result;
             InitializeComponent();
+        }
+
+        public void RefreshData()
+        {
+            _collection = ClientAPI.getCollection(_id).Result;
+            this.textBox2.Text = _collection.title;
+            this.label5.Text = _collection.id;
+            
+            for (int i = 0; i < _collection.contents.Count; i++)
+            {
+                this.textBox3.Text += _collection.id + " - " + _collection.title + "\n";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCollection_Load(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
