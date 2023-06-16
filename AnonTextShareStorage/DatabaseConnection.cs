5124250b194c5932cc8265f0753b7fa1126b5274
M Isa Al Anshori <isabungsu@gmail.com>
2023-06-16 18:30:43 +0700

    Clean Code: Remove unnecessary comments

--- AnonTextShareStorage/DatabaseConnection.cs
+++ AnonTextShareStorage/DatabaseConnection.cs
@@ -3,16 +3,16 @@
     public abstract class DatabaseConnection
     {
         // Document methods
-        public abstract string CreateDocument(string title, string text, KategoriDokumen.Kategori category); // return string id document, password isi null
-        public abstract string CreateDocument(string title, string text, KategoriDokumen.Kategori category, string pass); // return string id document, simpan password di hash
-        public abstract bool CheckDocument(string id); // return true jika dokumen ditemukan
-        public abstract bool CheckDocument(string id, string pass); // return true jika dokumen ditemukan dan pass benar
+        public abstract string CreateDocument(string title, string text, KategoriDokumen.Kategori category);
+        public abstract string CreateDocument(string title, string text, KategoriDokumen.Kategori category, string pass);
+        public abstract bool CheckDocument(string id);
+        public abstract bool CheckDocument(string id, string pass);
         public abstract string? GetDocumentTitle(string id);
         public abstract string? GetDocumentText(string id);
-        public abstract bool UpdateDocumentTitle(string id, string pass, string title); // return true jika title dokumen berhasil di update (dokumen ada dan pass benar)
-        public abstract bool UpdateDocumentText(string id, string pass, string contents); // return true jika teks dokumen berhasil di update (dokumen ada dan pass benar)
-        public abstract bool DeleteDocument(string id, string pass); // return true jika dokumen berhasil dihapus (dokumen ada dan pass benar)
-        public abstract int GetDocumentViews(string id); // Document Views di increment setiap GetDocumentText dipanggil
+        public abstract bool UpdateDocumentTitle(string id, string pass, string title);
+        public abstract bool UpdateDocumentText(string id, string pass, string contents);
+        public abstract bool DeleteDocument(string id, string pass);
+        public abstract int GetDocumentViews(string id);
         public abstract bool AddDocumentComment(string id, string text);
         public abstract List<string>? GetDocumentComments(string id);
         public abstract bool UpdateDocumentCategory(string id, KategoriDokumen.Kategori category, string pass);
@@ -24,17 +24,18 @@
         public abstract bool UnlockDocument(string id, string pass);
         public abstract int GetReportCount(string id);
         public abstract ReportingHabli.State? GetReportState(string id);
+
         // Collection methods
-        public abstract string CreateCollection(string title, List<string> contents); // return string id collection, password isi null
-        public abstract string CreateCollection(string title, List<string> contents, string pass); // return string id collection, simpan password di hash
-        public abstract bool CheckCollection(string id); // return true jika koleksi ditemukan
-        public abstract bool CheckCollection(string id, string pass); // return true jika koleksi ditemukan dan pass benar
+        public abstract string CreateCollection(string title, List<string> contents);
+        public abstract string CreateCollection(string title, List<string> contents, string pass);
+        public abstract bool CheckCollection(string id);
+        public abstract bool CheckCollection(string id, string pass);
         public abstract string? GetCollectionTitle(string id);
-        public abstract List<string>? GetCollectionContents(string id); // return list string id dokumen
-        public abstract bool UpdateCollectionTitle(string id, string title, string pass); // return true jika judul berhasil di ubah
-        public abstract bool AddCollectionDocument(string id, string documentId, string pass); // return true jika id dokumen berhasil ditambahkan
-        public abstract bool RemoveCollectionDocument(string id, string documentId, string pass); // return true jika id dokumen berhasil dihapus
-        public abstract bool DeleteCollection(string id, string pass); // return true jika koleksi berhasil di hapus
-        public abstract int GetCollectionViews(string id); // Collection Views di increment setiap GetCollectionContents dipanggil
+        public abstract List<string>? GetCollectionContents(string id);
+        public abstract bool UpdateCollectionTitle(string id, string title, string pass);
+        public abstract bool AddCollectionDocument(string id, string documentId, string pass);
+        public abstract bool RemoveCollectionDocument(string id, string documentId, string pass);
+        public abstract bool DeleteCollection(string id, string pass);
+        public abstract int GetCollectionViews(string id);
     }
 }
